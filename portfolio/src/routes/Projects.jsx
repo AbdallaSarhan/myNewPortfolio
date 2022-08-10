be534b72f4d77800f6911233894452962fd99fd6
AbdallaSarhan <abdalla1@my.yorku.ca>
2022-08-10 10:53:42 -0400

    Scroll to top of screen optimization

--- portfolio/src/routes/Projects.jsx
+++ portfolio/src/routes/Projects.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import HeroImg2 from "../components/HeroImg2";
 import Navbar from "../components/Navbar";
 import Project from "../components/Project";
 
 const Projects = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <Navbar />
