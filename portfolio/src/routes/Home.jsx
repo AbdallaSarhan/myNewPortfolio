96f2825d9fc95ac19ababc3ecadd90d41c90ec7f
AbdallaSarhan <abdalla1@my.yorku.ca>
2023-02-13 14:52:33 -0500

    homepage layout restructure

--- portfolio/src/routes/Home.jsx
+++ portfolio/src/routes/Home.jsx
@@ -3,13 +3,16 @@ import Navbar from "../components/Navbar";
 import HeroImg from "../components/HeroImg";
 import Footer from "../components/Footer";
 import ElevatorPitch from "../components/ElevatorPitch";
+import IntroSection from "../components/IntroSection";
+import SkillsSection from "../components/SkillsSection";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <HeroImg />
+      <IntroSection />
       <ElevatorPitch />
+      <SkillsSection />
       <Footer />
     </div>
   );
