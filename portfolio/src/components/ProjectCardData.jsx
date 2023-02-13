3fe434bf3482e557525f4740c096c0efc1eea3b4
AbdallaSarhan <abdalla1@my.yorku.ca>
2023-02-13 16:40:17 -0500

    adding new project

--- portfolio/src/components/ProjectCardData.jsx
+++ portfolio/src/components/ProjectCardData.jsx
@@ -2,20 +2,21 @@ import socialbook from "../assets/socialbook.png";
 import plant from "../assets/plant.png";
 import whereTo from "../assets/PostsScreen.png";
 import oldPortfolio from "../assets/oldPortfolio.png";
+import campUnity from "../assets/CampUnity.png";
 const ProjectCardData = [
   {
     id: 1,
     img: socialbook,
     title: "Socialbook",
-    text: "A social media platform where you can upload photos, add a description and category, save or comment on others photos. This website was built through ReactJS using HTML, Tailwind CSS, Javascript. Relevant libraries such as react-router-Dom and hooks such as UseState and UseEffect were used. The backend of this website was built using Sanity.io by creating schemas, Logging in with Google accomplished by Google cloud platform.",
+    text: "A social media platform where you can upload photos, add a description and category, save or comment on others photos. -Developed using React.JS and Tailwind CSS for styling. -Created the backend of this website using Sanity.io (CMS) using schemas. -Accomplsihed user authentication with Google using Google Cloud Platform.",
     view: "https://socialbookk.netlify.app",
     more: "",
   },
   {
     id: 2,
-    img: plant,
-    title: "Autonomous Plant Watering",
-    text: "This was a semester long project where we had an Arduino board in which soil moisture levels are measured consistently and below a certain level water automatically gets pumped through a tube to the plant. This project was created through Arduino IDE using C as well as IntelliJ IDE using Java.",
+    img: campUnity,
+    title: "CampUnity",
+    text: "Mobile application for university buy and sell marketplace -Developed Frontend using React Native CLI and Firebase for the backend (Firestore, Storage, Authentication). -Used Firestore to read and write users data, posts, and services. Utilized Firebase storage for storing images. -Created Custom React Hooks for different categories of data such as products, services, users.",
     view: "",
     more: "",
   },
@@ -23,7 +24,7 @@ const ProjectCardData = [
     id: 3,
     img: whereTo,
     title: "WhereTo",
-    text: "Consists of a login and registration system using JWT and PostgreSQL to store the data, in addition to hashing the passwords. Once you log in you can post trips and see who has posted trips. The frontend of this website was implemented with ReactJS and bootstrap for styling. The backend server is Express and nodeJS. Used React context to pass trips and render in different components. Axios for making requests to the server.",
+    text: "A travel sharing web application -Frontend implemented using React.JS and Bootstrap for styling. -Used React context to pass trips and render in different components. Axios for making requests to the server. -Backend server built using Express and Node.JS. -Accomplished login and registration system using JWT and PostgreSQL to store the data, in addition to hashing the passwords.",
     view: "",
     more: "",
   },
@@ -31,8 +32,16 @@ const ProjectCardData = [
     id: 4,
     img: oldPortfolio,
     title: "My Old Portfolio",
-    text: "This is my first Portfolio that I created which I have since decided to create a new one to reinforce some of my knowledge *Note that some data here may not be as accurate anymore",
+    text: "This is my first Portfolio. Note that some data here may not be updated anymore",
     view: "https://abdallasoldportfolio.netlify.app/",
+    more: "",
+  },
+  {
+    id: 5,
+    img: plant,
+    title: "Autonomous Plant Watering",
+    text: "This was a semester long project where we had an Arduino board in which soil moisture levels are measured consistently and below a certain level water automatically gets pumped through a tube to the plant.",
+    view: "",
     more: "",
   },
 ];
