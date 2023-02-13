f367876ec2b09994dacb3c137e70c104ef602512
AbdallaSarhan <abdalla1@my.yorku.ca>
2023-02-13 16:37:54 -0500

    content refactor

--- portfolio/src/components/ElevatorPitch.jsx
+++ portfolio/src/components/ElevatorPitch.jsx
@@ -17,6 +17,9 @@ const ElevatorPitch = () => {
     opacity: show ? 1 : 0,
     marginRight: show ? 0 : -710,
     color: show ? "white" : "black",
+    fontSize: 18,
+    fontWeight: 600,
+    paddingBottom: 20,
     // marginLeft: show ? -150 : -710,
   });
   useEffect(() => {
@@ -26,22 +29,29 @@ const ElevatorPitch = () => {
     <div className="container">
       <a.div className="summary-container" style={contentProps}>
         <a.div className="summary ">
-          <a.h4 ref={ref} style={contentProps}>
-            I'm a third year student studying a BEng specialized in Computer
-            Engineering at York University. My interests have led me in the
-            direction of full stack development and I've been learning and
-            experimenting with web and mobile development ever since.
-          </a.h4>
-          <a.h4 style={contentProps}>
+          <a.li ref={ref} style={contentProps}>
+            Third year student studying a BEng specialized in Computer
+            Engineering at York University.
+          </a.li>
+          <a.li ref={ref} style={contentProps}>
+            My interests have led me in the direction of full stack development
+            and I've been learning and experimenting with web and mobile
+            development ever since.
+          </a.li>
+          <a.li ref={ref} style={contentProps}>
             Naturally, I am a very curious person and love continuously learning
-            and filling the gaps in my understanding. I'm open to being involved
-            in many different roles to try new things and aquire new knowledge.
-          </a.h4>
-          <a.h4 style={contentProps}>
-            I think my eye for detail and determination for solving complex
+            and filling the gaps in my understanding.
+          </a.li>
+          <a.li ref={ref} style={contentProps}>
+            I'm open to being involved in many different roles to try new things
+            and aquire new knowledge.
+          </a.li>
+
+          <a.li style={contentProps}>
+            I believe my eye for detail and determination for solving complex
             problems will help me achieve my targets and be a valuable asset to
             any team.
-          </a.h4>
+          </a.li>
         </a.div>
       </a.div>
       <a.h1 className="title" style={headerProps}>
