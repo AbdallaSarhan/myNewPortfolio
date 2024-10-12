9e91f7da0129c5aeb2a49022e16c39cf75338ff2
Abdalla Sarhan <abdalla1@my.yorku.ca>
2024-10-12 14:06:12 -0400

    Adding GeoSnap project to projects

--- portfolio/src/components/ProjectCardData.jsx
+++ portfolio/src/components/ProjectCardData.jsx
@@ -3,47 +3,56 @@ import plant from "../assets/plant.png";
 import whereTo from "../assets/PostsScreen.png";
 import oldPortfolio from "../assets/oldPortfolio.png";
 import campUnity from "../assets/CampUnity.png";
+import pinMapDisplay from "../assets/geosnapAssets/pin-map-display.png";
 const ProjectCardData = [
-  {
-    id: 1,
-    img: socialbook,
-    title: "Socialbook",
-    text: "A social media platform where you can upload photos, add a description and category, save or comment on others photos. -Developed using React.JS and Tailwind CSS for styling. -Created the backend of this website using Sanity.io (CMS) using schemas. -Accomplsihed user authentication with Google using Google Cloud Platform.",
-    view: "https://socialbookk.netlify.app",
-    more: "",
-  },
-  {
-    id: 2,
-    img: campUnity,
-    title: "CampUnity",
-    text: "Mobile application for university buy and sell marketplace -Developed Frontend using React Native CLI and Firebase for the backend (Firestore, Storage, Authentication). -Used Firestore to read and write users data, posts, and services. Utilized Firebase storage for storing images. -Created Custom React Hooks for different categories of data such as products, services, users.",
-    view: "",
-    more: "",
-  },
-  {
-    id: 3,
-    img: whereTo,
-    title: "WhereTo",
-    text: "A travel sharing web application -Frontend implemented using React.JS and Bootstrap for styling. -Used React context to pass trips and render in different components. Axios for making requests to the server. -Backend server built using Express and Node.JS. -Accomplished login and registration system using JWT and PostgreSQL to store the data, in addition to hashing the passwords.",
-    view: "",
-    more: "",
-  },
-  {
-    id: 4,
-    img: oldPortfolio,
-    title: "My Old Portfolio",
-    text: "This is my first Portfolio. Note that some data here may not be updated anymore",
-    view: "https://abdallasoldportfolio.netlify.app/",
-    more: "",
-  },
-  {
-    id: 5,
-    img: plant,
-    title: "Autonomous Plant Watering",
-    text: "This was a semester long project where we had an Arduino board in which soil moisture levels are measured consistently and below a certain level water automatically gets pumped through a tube to the plant.",
-    view: "",
-    more: "",
-  },
+	{
+		id: 1,
+		img: pinMapDisplay,
+		title: "GeoSnap",
+		text: "Developed a mobile application for sharing photos based on location, similar to the iPhone Photos app's image map feature -Utilized React Native CLI for the front end and Node.js with MongoDB for the backend to manage data and functionality. -Integrated Amazon S3 for efficient and secure image storage and retrieval using presigned URLs.",
+		view: "",
+		more: "",
+	},
+	{
+		id: 2,
+		img: socialbook,
+		title: "Socialbook",
+		text: "A social media platform where you can upload photos, add a description and category, save or comment on others photos. -Developed using React.JS and Tailwind CSS for styling. -Created the backend of this website using Sanity.io (CMS) using schemas. -Accomplsihed user authentication with Google using Google Cloud Platform.",
+		view: "https://socialbookk.netlify.app",
+		more: "",
+	},
+	{
+		id: 3,
+		img: campUnity,
+		title: "CampUnity",
+		text: "Mobile application for university buy and sell marketplace -Developed Frontend using React Native CLI and Firebase for the backend (Firestore, Storage, Authentication). -Used Firestore to read and write users data, posts, and services. Utilized Firebase storage for storing images. -Created Custom React Hooks for different categories of data such as products, services, users.",
+		view: "",
+		more: "",
+	},
+	{
+		id: 4,
+		img: whereTo,
+		title: "WhereTo",
+		text: "A travel sharing web application -Frontend implemented using React.JS and Bootstrap for styling. -Used React context to pass trips and render in different components. Axios for making requests to the server. -Backend server built using Express and Node.JS. -Accomplished login and registration system using JWT and PostgreSQL to store the data, in addition to hashing the passwords.",
+		view: "",
+		more: "",
+	},
+	{
+		id: 5,
+		img: oldPortfolio,
+		title: "My Old Portfolio",
+		text: "This is my first Portfolio. Note that some data here may not be updated anymore",
+		view: "https://abdallasoldportfolio.netlify.app/",
+		more: "",
+	},
+	{
+		id: 6,
+		img: plant,
+		title: "Autonomous Plant Watering",
+		text: "This was a semester long project where we had an Arduino board in which soil moisture levels are measured consistently and below a certain level water automatically gets pumped through a tube to the plant.",
+		view: "",
+		more: "",
+	},
 ];
 
 export default ProjectCardData;
--- portfolio/src/routes/ProjectDetails.jsx
+++ portfolio/src/routes/ProjectDetails.jsx
@@ -10,69 +10,194 @@ import Dashboard from "../assets/Dashboard.png";
 import PostsScreen from "../assets/PostsScreen.png";
 import PostTripScreen from "../assets/PostTripScreen.png";
 import "../components/ProjectDetailStyles.css";
+import uploadProcessVideo from "../assets/Videos/uploadProcess.mp4";
+import locationRetrevialImg from "../assets/geosnapAssets/location-retrevial.png";
+import pinClusteringImg from "../assets/geosnapAssets/pin-clustering.png";
+import pinMapDisplay from "../assets/geosnapAssets/pin-map-display.png";
 import { useEffect } from "react";
 
 export const ProjectDetails = () => {
-  let { title } = useParams();
+	let { title } = useParams();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  if (title === "Autonomous Plant Watering") {
-    return (
-      <>
-        <Navbar />
-        <HeroImg2 heading="No More Details for" text={title} />
-        <Footer />
-      </>
-    );
-  } else if (title === "WhereTo") {
-    return (
-      <>
-        <Navbar />
-        <HeroImg2 heading={`Project Details: ${title}`} />
-        <div className="info">
-          <h4>This is the amazing looking home screen.</h4>
-          <img src={WelcomeScreen} alt="" className="projectImg" />
-          <h4>
-            The Login Requires a Registration with a valid email format
-            (example@example.example)
-          </h4>
-          <img src={LoginScreen} alt="" className="projectImg" />
-          <h4>
-            If you haven't Registered yet, a notification will pop up notifying
-            you that the server couldn't find you in the database.
-          </h4>
-          <img src={FailLogin} alt="" className="projectImg" />
-          <h4>
-            Once you login or register succesfully you are Welcomed with the
-            dashboard (Note: Test is the name of the logged in user in this case
-            which is why "test" shows as the name under welcome)
-          </h4>
-          <img src={Dashboard} alt="" className="projectImg" />
-          <h4>This is where you are able to post your trip details</h4>
-          <img src={PostTripScreen} alt="" className="projectImg" />
-          <h4>
-            You can come and see who has posted their trip details. Note that
-            the "Allowing" section of the posts was a an idea that I've decided
-            to migrate to a different project that I'm currently working on
-          </h4>
-          <img src={PostsScreen} alt="" className="projectImg" />
-        </div>
+	if (title === "Autonomous Plant Watering") {
+		return (
+			<>
+				<Navbar />
+				<HeroImg2 heading="No More Details for" text={title} />
+				<Footer />
+			</>
+		);
+	} else if (title === "WhereTo") {
+		return (
+			<>
+				<Navbar />
+				<HeroImg2 heading={`Project Details: ${title}`} />
+				<div className="info">
+					<h4>This is the amazing looking home screen.</h4>
+					<img src={WelcomeScreen} alt="" className="projectImg" />
+					<h4>
+						The Login Requires a Registration with a valid email format
+						(example@example.example)
+					</h4>
+					<img src={LoginScreen} alt="" className="projectImg" />
+					<h4>
+						If you haven't Registered yet, a notification will pop up notifying
+						you that the server couldn't find you in the database.
+					</h4>
+					<img src={FailLogin} alt="" className="projectImg" />
+					<h4>
+						Once you login or register succesfully you are Welcomed with the
+						dashboard (Note: Test is the name of the logged in user in this case
+						which is why "test" shows as the name under welcome)
+					</h4>
+					<img src={Dashboard} alt="" className="projectImg" />
+					<h4>This is where you are able to post your trip details</h4>
+					<img src={PostTripScreen} alt="" className="projectImg" />
+					<h4>
+						You can come and see who has posted their trip details. Note that
+						the "Allowing" section of the posts was a an idea that I've decided
+						to migrate to a different project that I'm currently working on
+					</h4>
+					<img src={PostsScreen} alt="" className="projectImg" />
+				</div>
 
-        <Footer />
-      </>
-    );
-  } else if (title === "CampUnity") {
-    return (
-      <>
-        <>
-          <Navbar />
-          <HeroImg2 heading="No More Details for" text={title} />
-          <Footer />
-        </>
-      </>
-    );
-  }
+				<Footer />
+			</>
+		);
+	} else if (title === "CampUnity") {
+		return (
+			<>
+				<>
+					<Navbar />
+					<HeroImg2 heading="No More Details for" text={title} />
+					<Footer />
+				</>
+			</>
+		);
+	} else if (title === "GeoSnap") {
+		return (
+			<>
+				<div style={{}}>
+					<Navbar />
+					<HeroImg2 heading={`Project Details: ${title}`} />
+					<div className="simulator-screenshots-container">
+						<div
+							style={{
+								alignItems: "center",
+								justifyContent: "center",
+								display: "flex",
+								flexDirection: "column",
+							}}
+						>
+							<h3 style={{ color: "black" }}>Current Location Retrevial</h3>
+							<p
+								style={{
+									color: "black",
+									marginBottom: 40,
+									marginTop: 10,
+									fontSize: 14,
+									maxWidth: "300px",
+									textAlign: "center",
+								}}
+							>
+								Once the app is launched, the user's current location requested
+								and then retreived.
+							</p>
+							<img
+								src={locationRetrevialImg}
+								alt="location retrevial image"
+								className="image"
+							/>
+						</div>
+						<div
+							style={{
+								alignItems: "center",
+								justifyContent: "center",
+								display: "flex",
+								flexDirection: "column",
+							}}
+						>
+							<h3 style={{ color: "black" }}>Pin Clustering</h3>
+							<p
+								style={{
+									color: "black",
+									marginBottom: 40,
+									marginTop: 10,
+									fontSize: 14,
+									maxWidth: "300px",
+									textAlign: "center",
+								}}
+							>
+								Pins are clustered together when the user is zoomed out.
+							</p>
+							<img
+								src={pinClusteringImg}
+								alt="Pin clustering image"
+								className="image"
+							/>
+						</div>
+						<div
+							style={{
+								alignItems: "center",
+								justifyContent: "center",
+								display: "flex",
+								flexDirection: "column",
+							}}
+						>
+							<h3 style={{ color: "black" }}>Displaying Pins on the Map</h3>
+							<p
+								style={{
+									color: "black",
+									marginBottom: 40,
+									marginTop: 10,
+									fontSize: 14,
+									maxWidth: "300px",
+									textAlign: "center",
+								}}
+							>
+								After the user zooms in, the pins will be displayed on the map.
+							</p>
+							<img
+								src={pinMapDisplay}
+								alt="displaying pins on the map"
+								className="image"
+							/>
+						</div>
+					</div>
+					<div className="video-container">
+						<h2 style={{ color: "black", marginBottom: 10 }}>
+							Upload Process Video
+						</h2>
+						<p
+							style={{
+								color: "black",
+								marginBottom: 40,
+								marginTop: 10,
+								fontSize: 14,
+								maxWidth: "1000px",
+								textAlign: "center",
+							}}
+						>
+							After the user zooms in, the pins will be displayed on the map.
+						</p>
+						<video
+							width="750"
+							height="500"
+							controls
+							className="video"
+							onLoadedMetadata={(e) => (e.target.playbackRate = 2)}
+						>
+							<source src={uploadProcessVideo} type="video/mp4" />
+						</video>
+					</div>
+
+					<Footer />
+				</div>
+			</>
+		);
+	}
 };
